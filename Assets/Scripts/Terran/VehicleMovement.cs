bd11dfe20b53ba6d8389671ee69d09860d911802
spyjack <terranorion@gmail.com>
2020-04-04 18:27:15 -0400

    Made a worker scriptable object

---
+++ Assets/Scripts/Terran/VehicleMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class VehicleMovement : MonoBehaviour
+{
+    [SerializeField]
+    private List<Vector3> targetQueue = new List<Vector3>();
+
+    [SerializeField]
+    private float speed = 200f;
+
+    [SerializeField]
+    private float nextWaypointDistance = 3f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    [SerializeField]
+    Seeker seeker;
+
+    [SerializeField]
+    Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    WorkerBase worker = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdatePath", 0f, 1f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            targetQueue.Add(mousePos);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rigidbody2D.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rigidbody2D.AddForce(force);
+
+        float distance = Vector2.Distance(rigidbody2D.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone() && targetQueue.Count > 0)
+        seeker.StartPath(rigidbody2D.position, targetQueue[0], OnPathComplete);
+    }
+
+    void OnPathComplete(Path _path)
+    {
+        if (!_path.error)
+        {
+            path = _path;
+            currentWaypoint = 0;
+        }
+        targetQueue.RemoveAt(0);
+    }
+}
---
+++ Assets/Scripts/Terran/WorkerBase.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Worker", menuName = "Worker", order = 51)]
+public class WorkerBase : ScriptableObject
+{
+    [SerializeField]
+    string _workerName = "";
+
+    [SerializeField]
+    Sprite _portrait = null;
+
+    [SerializeField]
+    Gender _gender = Gender.Male;
+
+    [SerializeField]
+    WorkStation _equippedStation = WorkStation.None;
+
+    [SerializeField]
+    int _boost1 = 0;
+
+    public string WorkerName
+    {
+        get { return _workerName; }
+        set { _workerName = value; }
+    }
+
+    public Sprite Portrait
+    {
+        get { return _portrait; }
+        set { _portrait = value; }
+    }
+
+    public Gender Gender
+    {
+        get { return _gender; }
+        set { _gender = value; }
+    }
+}
+
+public enum Gender
+{
+    Male,
+    Female
+}
+
+public enum WorkStation
+{
+    None,
+    Cabin,
+    Engine,
+    Drill,
+    Equipment
+}
