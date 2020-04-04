9405cda4d9d6576d2a6270052cc8a922c28804ea
spyjack <terranorion@gmail.com>
2020-04-03 23:51:54 -0400

    Rearranging files, hopefully it works.
    We should be committing straight to the file, there should be no zipping of projects, it should just auto update here.

---
+++ Assets/2D Platformer Controller/Scripts/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerInput : MonoBehaviour
+{
+    private Player player;
+    public Animator anim;
+    public float speed;
+    public Rigidbody2D playerRigid;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        
+        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        player.SetDirectionalInput(directionalInput);
+        speed = playerRigid.inertia;
+        anim.SetFloat("Speed", speed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            player.OnJumpInputDown();
+            anim.SetBool("Jump", true);
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            player.OnJumpInputUp();
+            anim.SetBool("Jump", false);
+        }
+
+        
+    }
+}
