f59390f539bd3674282be1fc6ebc7aacb12fb945
willgander <36284896+willgander@users.noreply.github.com>
2020-04-06 22:46:54 -0400

    Add files via upload
    Attempt at terrain destruction

---
+++ Assets/Scripts/William/askdjbfiawfh.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class askdjbfiawfh : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Cube")
+        {
+            tilemap.SetTile(new Vector3Int(-7, 3, 0), null);
+            print("HIT THE GROUND");
+        }
+        Vector3 hitPosition = Vector3.zero;
+        foreach (ContactPoint2D hit in collision.contacts)
+        {
+            Debug.Log(hit.point);
+            hitPosition.x = hit.point.x - 0.1f;
+            hitPosition.y = hit.point.y - 0.1f;
+            tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+        }
+    }
+}
