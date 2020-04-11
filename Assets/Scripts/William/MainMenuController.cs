8775773cf845352e3933edbf89de2f41a233df36
willgander <36284896+willgander@users.noreply.github.com>
2020-04-11 19:43:55 -0400

    Main Menu created
    Also the Main Menu is interactive, just left click to start, left click again to increase land value, right click to clear all land to restart.
    
    4/11/2020

---
+++ Assets/Scripts/William/MainMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject helpPanel;
+    public GameObject creditsPanel;
+    [Header("Scene Info")]
+    public string nextSceneName;
+
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+    public void OnClickQuit()
+    {
+        Application.Quit();
+        print("Application is trying to close");
+    }
+    public void OnClickHelp()
+    {
+        helpPanel.gameObject.SetActive(true);
+    }
+    public void OnClickCredits()
+    {
+        creditsPanel.gameObject.SetActive(true);
+    }
+    public void OnClickHelpQuit()
+    {
+        helpPanel.gameObject.SetActive(false);
+    }
+    public void OnClickCreditsQuit()
+    {
+        creditsPanel.gameObject.SetActive(false);
+    }
+}
