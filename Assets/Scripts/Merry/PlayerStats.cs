2cf49453419a42745653f6c9ef96d6dfa59369ee
MerryMcl <55710884+MerryMcl@users.noreply.github.com>
2020-04-18 12:51:22 -0400

    Here has art & a base for the UI

---
+++ Assets/Scripts/Merry/PlayerStats.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static PlayerStats arc;
+
+    public int lv;
+
+    public float Cash = 100;
+
+    public float drilWork;
+
+    public bool[] Wheels = new bool[3] {true, false, false };
+    public bool[] Drill = new bool[3] { true, false, false };
+    public GameObject[] Base = new GameObject[3];
+    public GameObject[] Observitory = new GameObject[3];
+
+   public string[] Name = new string[3] { "Sam", "Dean", "Cass"};
+
+    public int[] Work = new int[3] { 4, 4, 4};
+
+    public Animator Wheel;
+    public int num;
+    [SerializeField] private KeyCode moveKey;
+
+    private void Awake()
+    {
+        Wheel.SetInteger("Wheel", 0);
+        arc = this;
+        lv = 1;
+    }
+    public void Start()
+    {
+        ChangeBase(0);
+        ChangeObservitory(0);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(moveKey))
+        {
+            Move();
+        }
+    }
+    public void WorkTotal()
+    {
+        if(lv == 1)
+        {
+            drilWork = 2;
+            for (int i = 0; i > lv; i++)
+            {
+                drilWork += Work[i];
+            }
+        }
+        if (lv == 2)
+        {
+            drilWork = 4;
+            for (int i = 0; i > lv; i++)
+            {
+                drilWork += Work[i];
+            }
+        }
+        if (lv == 3)
+        {
+            drilWork = 6;
+            for (int i = 0; i > lv; i++)
+            {
+            drilWork += Work[i];
+            }
+        }
+
+
+    }
+
+    public void ChangeWeels(int num)
+    {
+         if(num == 0)
+        {
+            Wheel.SetInteger("Wheel", 0);
+            Wheels[0] = true;
+            Wheels[1] = false;
+            Wheels[2] = false;
+        }
+
+        if (num == 1)
+        {
+            Wheel.SetInteger("Wheel", 1);
+            Wheels[0] = false;
+            Wheels[1] = true;
+            Wheels[2] = false;
+        }
+
+        if (num == 2)
+        {
+            Wheel.SetInteger("Wheel", 3);
+            Wheels[0] = false;
+            Wheels[1] = false;
+            Wheels[2] = true;
+        }
+    }
+
+    public void ChangeBase(int num)
+    {
+        if(num == 0)
+        {
+            Base[0].SetActive(true);
+            Base[1].SetActive(false);
+            Base[2].SetActive(false);
+        }
+        if (num == 1)
+        {
+            Base[0].SetActive(false);
+            Base[1].SetActive(true);
+            Base[2].SetActive(false);
+        }
+        if (num == 2)
+        {
+            Base[0].SetActive(false);
+            Base[1].SetActive(false);
+            Base[2].SetActive(true);
+        }
+    }
+
+    private void Move()
+    {        
+        num++;
+        if (num == 1)
+        {
+            Wheel.SetBool("Moveing", false);
+        }
+        if (num == 2)
+        {
+            Wheel.SetBool("Moveing", true);
+            num = 0;
+        }
+    }
+
+    public void ChangeDrill(int num)
+    {
+        if (num == 0)
+        {
+            Drill[0] = true;
+            Drill[1] = false;
+            Drill[2] = false;
+        }
+        if (num == 1)
+        {
+            Drill[0] = false;
+            Drill[1] = true;
+            Drill[2] = false;
+        }
+        if (num == 2)
+        {
+            Drill[0] = false;
+            Drill[1] = false;
+            Drill[2] = true;
+        }
+    }
+
+    public void ChangeObservitory(int num)
+    {
+        if (num == 0)
+        {
+            Observitory[0].SetActive(true);
+            Observitory[1].SetActive(false);
+            Observitory[2].SetActive(false);
+        }
+        if (num == 1)
+        {
+            Observitory[0].SetActive(false);
+            Observitory[1].SetActive(true);
+            Observitory[2].SetActive(false);
+        }
+        if (num == 2)
+        {
+            Observitory[0].SetActive(false);
+            Observitory[1].SetActive(false);
+            Observitory[2].SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Merry/UIController.cs
@@ -0,0 +1,318 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class UIController : MonoBehaviour
+{
+    [Header ("InGame")]//////////////////
+    public Text lvText;
+
+    public GameObject leavePanel;
+    public GameObject shopPanel;
+    public GameObject menuPanel;
+
+   
+//    public Text[] statsText;
+    //public GameObject statsButton;
+
+    [Header("Shop")]//////////////////////
+    public Text ShopCashText;
+    public Text ShopText;
+
+    public GameObject wheelsPanel;
+    public GameObject basePanel;
+    public GameObject drillPanel;
+    public GameObject obPanel;
+
+    [Header("Stats")]
+    public GameObject[] workerStatsPanel = new GameObject[3];
+    public Text[] statsText;
+
+    [Header("Text")]
+
+
+    public Text DrilSatsText;
+
+
+    [Header("Misk")]
+    public int level;
+    private int statscount;
+    
+
+    private void Awake()
+    {
+       
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statscount = 1;
+        leavePanel.SetActive(false);
+        shopPanel.SetActive(false);
+        menuPanel.SetActive(false);
+        Debug.Log("Check1");
+        lvText.text = "Lv: " + PlayerStats.arc.lv;
+        Debug.Log("Check2");
+/*        workerStatsPanel[0].SetActive(false);
+        workerStatsPanel[1].SetActive(false);
+        workerStatsPanel[2].SetActive(false);
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //nMouseOver();
+    }
+
+    // leval
+    
+
+        public void GetLevel(int lv)
+    {
+        level = lv;
+    }
+
+    public void LvUpButtom()
+    {
+        level = PlayerStats.arc.lv;
+       // level = playerStats.arc.playerLevel;
+        if(level <=2)
+        {
+            //PlayerStats.instance.UpdateLevel(true);
+           PlayerStats.arc.lv++;
+           lvText.text = "Lv: " + PlayerStats.arc.lv;
+        }
+        else
+        {
+            
+        }
+    }
+    public void LvDownButtom()
+    {
+
+        level = PlayerStats.arc.lv;
+        if (level >= 1)
+        {
+            PlayerStats.arc.lv--;
+            lvText.text = "Lv: " + PlayerStats.arc.lv;
+        }
+        else
+        {
+        }
+    }
+    private void LevelText()
+    {
+
+    }
+    
+
+    //worker stats panel
+    /*
+
+    public void statsShowButton()
+    {
+        Debug.Log("Clicked");
+        statscount++;
+        if(statscount == 2)
+        {
+            Debug.Log("2");
+
+            level = PlayerStats.arc.lv;
+            for (int i = 0; i < level; i++)
+            {
+                workerStatsPanel[i].SetActive(true);
+            }
+        }
+        if (statscount == 3)
+        {
+            Debug.Log("3");
+
+            
+            for (int i = 0; i < 3; i++)
+            {
+                workerStatsPanel[i].SetActive(false);
+            }
+            statscount = 1;
+        }
+    }  
+*/
+
+    //leave Panel -- DONE
+
+    public void OnLeaveButton()
+    {
+        leavePanel.SetActive(true);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    public void DontQuitButton()
+    {
+        leavePanel.SetActive(false);
+    }
+
+
+    //menu panel
+    public void goToMenu()
+    {
+        menuPanel.SetActive(true);
+
+        PlayerStats.arc.WorkTotal();
+        DrilSatsText.text = "Work: " + PlayerStats.arc.drilWork + " \n" + "Money: " + PlayerStats.arc.Cash;
+
+        Debug.Log("Now Name");
+
+        //string statText = "";
+        for (int i = 0; i < PlayerStats.arc.lv; i++)
+        {
+            Debug.Log("" + i );
+            statsText[i].text = "Name: " + PlayerStats.arc.Name[i] + "\n" + "Work Contribut: " + PlayerStats.arc.Work[i];
+        }
+
+    }
+    public void leaveToMenu()
+    {
+        menuPanel.SetActive(false);
+    }
+
+
+    //shop panel
+    public void goToShopButton()
+    {
+        shopPanel.SetActive(true);
+        ShopCashText.text = "Your Cash $" + PlayerStats.arc.Cash;
+        ShopText.text = "Everything cost $25.";
+
+    }
+
+    public void leaveShopButton()
+    {
+        shopPanel.SetActive(false);
+    }
+    public void MoreCash()
+    {
+        float num = 50;
+        PlayerStats.arc.Cash += num;
+        ShopCashText.text = "Your Cash $" + PlayerStats.arc.Cash;
+    }
+
+    //Wheels Buttons
+
+    public void W0Button()
+    {
+        CanBuy(1, 0);
+    }
+    public void W1Button()
+    {
+        CanBuy(1, 1);
+    }
+    public void W2Button()
+    {
+        CanBuy(1, 2);
+    }
+
+    //Base Buttons
+    public void Base0Button()
+    {
+        CanBuy(4, 0);
+    }
+    public void Base1Button()
+    {
+        CanBuy(4, 1);
+    }
+    public void Base2Button()
+    {
+        CanBuy(4, 2);
+    }
+
+    //Drill Buttons
+    //Observitory Buttons
+    public void Ob0Button()
+    {
+        CanBuy(3, 0);
+    }
+    public void Ob1Button()
+    {
+        CanBuy(3, 1);
+    }
+    public void Ob2Button()
+    {
+        CanBuy(3, 2);
+    }
+
+    //Can buy?
+
+    private void CanBuy(int what, int lv)
+    {
+        int check = 25;
+        if(check <= PlayerStats.arc.Cash)
+        {
+            //Wheels
+            if (what == 1 && lv == 0)
+            {
+                PlayerStats.arc.ChangeWeels(0);
+            }
+            if (what == 1 && lv == 1)
+            {
+                PlayerStats.arc.ChangeWeels(1);
+            }
+            if (what == 1 && lv == 2)
+            {
+                PlayerStats.arc.ChangeWeels(2);
+            }
+            //Drill
+            if (what == 2 && lv == 0)
+            {
+
+            }
+            if (what == 2 && lv == 1)
+            {
+
+            }
+            if (what == 2 && lv == 2)
+            {
+
+            }
+            //Observitory
+            if (what == 3 && lv == 0)
+            {
+                PlayerStats.arc.ChangeObservitory(0);
+            }
+            if (what == 3 && lv == 1)
+            {
+                PlayerStats.arc.ChangeObservitory(1);
+            }
+            if (what == 3 && lv == 2)
+            {
+                PlayerStats.arc.ChangeObservitory(2);
+            }
+            //Base
+            if (what == 4 && lv == 0)
+            {
+                PlayerStats.arc.ChangeBase(0);
+            }
+            if (what == 4 && lv == 1)
+            {
+                PlayerStats.arc.ChangeBase(1);
+            }
+            if (what == 4 && lv == 2)
+            {
+                PlayerStats.arc.ChangeBase(2);
+            }
+
+            PlayerStats.arc.Cash -= 25;
+            ShopCashText.text = "Your Cash $" + PlayerStats.arc.Cash;
+            ShopText.text = "Everything cost $25.";
+        }
+        else
+        {
+            ShopText.text = "You don't have enoughfe money. Everything cost $25.";
+        }
+    }
+}
