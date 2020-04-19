d4627aeaf2d0d1866c9f6bd43eb0dcda42cb5772
spyjack <terranorion@gmail.com>
2020-04-18 23:18:56 -0400

    Depth Meter

---
+++ Assets/Scripts/Terran/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private int money = 100;
+
+    [SerializeField]
+    private List<WorkerBase> workerInventory = new List<WorkerBase>();
+
+    [SerializeField]
+    private List<PartBase> partsInventory = new List<PartBase>();
+
+    [SerializeField]
+    private float deepestDepth = 0;
+
+    [SerializeField]
+    private List<Transform> playerShips = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetAllShips();
+        StartCoroutine(CheckDepths());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CheckDepths()
+    {
+        while(true)
+        {
+            
+            float lowest = deepestDepth;
+            foreach (Transform vehicle in playerShips)
+            {
+                if (vehicle.position.y * 4 < lowest)
+                {
+                    lowest = vehicle.position.y * 4;
+                }
+            }
+            print("Checking Depths " + lowest);
+            deepestDepth = lowest;
+            yield return new WaitForSeconds(0.25f * playerShips.Count);
+        }
+    }
+
+    void GetAllShips()
+    {
+        foreach (VehicleClass vehicle in GameObject.FindObjectsOfType<VehicleClass>())
+        {
+            playerShips.Add(vehicle.GetComponent<Transform>());
+        }
+    }
+}
