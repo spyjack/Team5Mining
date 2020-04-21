f5857ed2a48dcd573cf439bebf2b1d156909274d
spyjack <terranorion@gmail.com>
2020-04-20 20:50:24 -0400

    Changes to Official GameScene
    Also added TextMeshPro but I don't know how to use it so I'm holding off

--- Assets/Scripts/Terran/PlayerController.cs
+++ Assets/Scripts/Terran/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,6 +16,20 @@ public class PlayerController : MonoBehaviour
     private List<PartBase> partsInventory = new List<PartBase>();
 
     [SerializeField]
+    GameObject depthLabel = null;
+
+    [SerializeField]
+    Image depthTextBreak = null;
+
+    [SerializeField]
+    List<Text> depthTextsList = new List<Text>();
+
+    [SerializeField]
+    private List<DepthMarker> depthMarkers = new List<DepthMarker>();
+
+    private int nextDepth = 0;
+
+    [SerializeField]
     private float deepestDepth = 0;
 
     [SerializeField]
@@ -24,6 +39,10 @@ public class PlayerController : MonoBehaviour
     AstarPath astar = null;
 
     public bool dirtyNav = true;
+
+    [SerializeField]
+    BoxCollider2D camBounds = null;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -53,6 +72,21 @@ public class PlayerController : MonoBehaviour
             }
             //print("Checking Depths " + lowest);
             deepestDepth = lowest;
+            if (deepestDepth <= depthMarkers[nextDepth].depthLevel)
+            {
+                depthTextsList[1].text = depthMarkers[nextDepth].depthName;
+                depthTextsList[2].text = depthMarkers[nextDepth].depthLevel*-1 + " m";
+                //offset is half of the increased amount
+                Vector2 newOffset = camBounds.offset;
+                Vector2 newSize = camBounds.size;
+                float newYSize = Mathf.Abs(depthMarkers[nextDepth + 1].depthLevel * 0.25f);
+                newSize.y += newYSize;
+                newOffset.y -= (newYSize * 0.5f);
+                camBounds.size = newSize;
+                camBounds.offset = newOffset;
+                StartCoroutine(DisplayDepthText());
+                nextDepth++;
+            }
             yield return new WaitForSeconds(0.25f * playerShips.Count);
         }
     }
@@ -70,6 +104,35 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    IEnumerator DisplayDepthText()
+    {
+        depthLabel.SetActive(true);
+        Color labelColor = new Color(1, 1, 1, 0);
+        while (labelColor.a < 1)
+        {
+            labelColor.a += 0.1f;
+            depthTextBreak.color = labelColor;
+            foreach (Text text in depthTextsList)
+            {
+                text.color = labelColor;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(1f);
+        while (labelColor.a > 0)
+        {
+            labelColor.a -= 0.25f;
+            depthTextBreak.color = labelColor;
+            foreach (Text text in depthTextsList)
+            {
+                text.color = labelColor;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        depthLabel.SetActive(false);
+        yield break;
+    }
+
     void GetAllShips()
     {
         foreach (VehicleClass vehicle in GameObject.FindObjectsOfType<VehicleClass>())
@@ -78,3 +141,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 }
+
+[System.Serializable]
+public struct DepthMarker
+{
+    
+    public float depthLevel;
+    
+    public string depthName;
+}
+ 
+
